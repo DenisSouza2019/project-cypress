7e8377a93ead29985009650ae92aea1dd75fc5c1
DR <denis.souza.rosa@gmail.com>
2024-07-21 15:05:32 -0300

    config .yml cloud

--- study-cypress-two/cypress.config.js
+++ study-cypress-two/cypress.config.js
@@ -6,7 +6,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    video:true,
     reporter:'mochawesome',
     reporterOptions: {
       reportDir: 'cypress/reports',
