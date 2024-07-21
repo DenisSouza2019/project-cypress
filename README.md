fdf37c1a75634fa0f035b2e46e9b9995c4296edd
DR <denis.souza.rosa@gmail.com>
2024-07-21 13:50:56 -0300

    config reports cypress

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Estudos sobre cypress, analisando metodologia e aplicabilidades.
 
 1. study-cypress: Treinamento 1
 2. study-cypress-two: Treinamento 2 
+    - mochawesome (Gerado de relatorio para cypress) -> https://www.npmjs.com/package/mochawesome
--- study-cypress-two/cypress.config.js
+++ study-cypress-two/cypress.config.js
@@ -1,10 +1,22 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "xoijyt",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     video:true,
+    reporter:'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      reportFilename: 'results-test-reports',
+      quiet: true,
+      overwrite: false,
+      html: true,
+      json: false,
+      timestamp: 'dd-mm-yyyy',
+    },
+
   },
 });
