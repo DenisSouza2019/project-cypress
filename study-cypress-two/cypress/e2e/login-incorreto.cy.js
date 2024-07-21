3f0496b16a580560e9fb10f6386245ee202c0d76
DR <denis.souza.rosa@gmail.com>
2024-07-21 15:29:28 -0300

    config. cloud cypress

--- study-cypress-two/cypress/e2e/login-incorreto.cy.js
+++ study-cypress-two/cypress/e2e/login-incorreto.cy.js
@@ -7,9 +7,7 @@ describe('Página de login', () => {
     })
 
     it('Deve preencher os campos do login corretamente e autenticar o usuário na página', () => {
-      // cy.get('[data-test="input-loginEmail"]').type('ana@email.com');
-      // cy.get('[data-test="input-loginPassword"]').type('123');
-      // cy.get('[data-test="submit-button"]').click();
+
       cy.loginPersonalizado('ana@email.com', '123');
       cy.get('.error').should('have.text', 'A senha deve conter pelo menos uma letra maiúscula, um número e ter entre 6 e 15 caracteres');
     })
